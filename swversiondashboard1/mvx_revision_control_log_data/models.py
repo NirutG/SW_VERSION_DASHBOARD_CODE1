786a93fcb9682711fa464a26f40b9e9a4af28422
NirutG <nirut.system@gmail.com>
2021-12-09 10:51:52 +0700

    Create 2 tables in app: mvx_revision_control_log

--- swversiondashboard1/mvx_revision_control_log_data/models.py
+++ swversiondashboard1/mvx_revision_control_log_data/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class InlineActualVersion(models.Model):
+    line = models.CharField(max_length=35, default='', null=True)
+
+    exe_0950_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_0950_actual_status = models.CharField(max_length=35, default='', null=True)
+    plc_0950_actual_version = models.CharField(max_length=35, default='', null=True)
+    plc_0950_actual_status = models.CharField(max_length=35, default='', null=True)
+    hmi_0950_actual_version = models.CharField(max_length=35, default='', null=True)
+    hmi_0950_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    exe_1560_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1560_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    plc_1580_actual_version = models.CharField(max_length=35, default='', null=True)
+    plc_1580_actual_status = models.CharField(max_length=35, default='', null=True)
+    hmi_1580_actual_version = models.CharField(max_length=35, default='', null=True)
+    hmi_1580_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    plc_1586_actual_version = models.CharField(max_length=35, default='', null=True)
+    plc_1586_actual_status = models.CharField(max_length=35, default='', null=True)
+    hmi_1586_actual_version = models.CharField(max_length=35, default='', null=True)
+    hmi_1586_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    exe_1587_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1587_actual_status = models.CharField(max_length=35, default='', null=True)
+    plc_1587_actual_version = models.CharField(max_length=35, default='', null=True)
+    plc_1587_actual_status = models.CharField(max_length=35, default='', null=True)
+    hmi_1587_actual_version = models.CharField(max_length=35, default='', null=True)
+    hmi_1587_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    exe_1595_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1595_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    exe_1596_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1596_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    exe_1750_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1750_actual_status = models.CharField(max_length=35, default='', null=True)
+
+    
+class OfflineActualVersion(models.Model):
+    line = models.CharField(max_length=35, default='', null=True)
+
+    exe_1600_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1600_actual_status = models.CharField(max_length=35, default='', null=True)
+    
+    exe_1700_actual_version = models.CharField(max_length=35, default='', null=True)
+    exe_1700_actual_status = models.CharField(max_length=35, default='', null=True)
